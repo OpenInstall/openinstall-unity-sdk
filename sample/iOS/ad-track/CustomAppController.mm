7e001f478998b44cf9d5a6e5aaf84a1b75ebc3b6
loadingCooper <418489104@qq.com>
2020-07-21 18:41:08 +0800

    ad track

---
+++ sample/iOS/ad-track/CustomAppController.mm
@@ -0,0 +1,57 @@
+//
+//  CustomAppController.m
+//  Unity-iPhone
+//
+//  Created by cooper on 2018/6/15.
+//
+
+#import "UnityAppController.h"
+#import "OpenInstallSDK.h"
+#import "OpenInstallUnity3DCallBack.h"
+#include "PluginBase/AppDelegateListener.h"
+#import <AdSupport/AdSupport.h>
+
+@interface CustomAppController : UnityAppController<AppDelegateListener>
+
+@end
+
+//如果用户使用了IMPL_APP_CONTROLLER_SUBCLASS宏生成自己的customAppController,请删除该文件，并在自己的customAppController中添加如下初始化和拉起回调，或者按照官网iOS原生的集成方法集成
+IMPL_APP_CONTROLLER_SUBCLASS(CustomAppController)
+
+@implementation CustomAppController
+
+-(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+    
+    NSString *idfaStr = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+    [OpenInstallSDK initWithDelegate:[OpenInstallUnity3DCallBack defaultManager] advertisingId:idfaStr];
+    
+    UnityRegisterAppDelegateListener(self);
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    return YES;
+}
+
+- (void)onOpenURL:(NSNotification *)notification{
+    if (notification.userInfo && [notification.userInfo objectForKey:@"url"]) {
+        NSURL *url = [notification.userInfo objectForKey:@"url"];
+        [OpenInstallSDK handLinkURL:url];
+    }
+}
+
+-(BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler{
+//    [self configOrientation];//部分老版本unity，需要在拉起时进行强制横屏，新版本unity中可能会导致拉起崩溃，建议关闭，具体情况根据测试来定
+    
+    [OpenInstallSDK continueUserActivity:userActivity];
+    Class unity = NSClassFromString(@"UnityAppController");
+    if ([unity instancesRespondToSelector:@selector(application:continueUserActivity:restorationHandler:)]) {
+        [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+    }
+    return YES;
+}
+
+- (void)configOrientation{
+    NSNumber *orientationTarget = [NSNumber numberWithInt:UIInterfaceOrientationLandscapeLeft];
+    [[UIDevice currentDevice] setValue:orientationTarget forKey:@"orientation"];
+}
+
+@end
